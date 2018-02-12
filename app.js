b486f1d898af67173a1209639f3380fb301c8040
wael ali <waelali595@gmail.com>
2018-02-12 23:37:13 +0100

    adding eventListeners

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 // Module patterns 
-//Module one
+// BUDGET CONTROOLER
 var budgetController = (function(){
 
   // returnobject of our public methods
-  return {
-    publicFunction: function(){}
-  }
+
 })();
 
 
 
 
 
-// Module two
+// UI CONTROLLER
 var UIController = (function(){
 
 
 
 
   // public area
-  return {
-    publicCtrlfunction: function(){}
-  }
+
 })();
 
 
@@ -32,8 +28,35 @@ var UIController = (function(){
 
 
 // Module three => the main area => it connects othr controllers
+// GLOBAL APP CONTROLLER
 
 var controller = (function(budgetCtrl, UICtrlr){
+
+  var ctrlAddItem = function(){
+      console.log('It works!');
+      // get the field input 
+
+      // add the item to the budget controller
+
+      // add the item to the UI
+
+      // calculate the budget 
+
+      // display the budget on the UI 
+
+     
+  };
+
+  document.querySelector(".add-btn").addEventListener('click', function(){
+    ctrlAddItem();
+  });
+
+  document.addEventListener('keypress', function(event){
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+
 
 
 
