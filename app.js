a27cb607befa599f2ba65eeaf4b6bb3b5df1c44a
wael ali <waelali595@gmail.com>
2018-02-13 23:00:11 +0100

    initialization function

--- app.js
+++ app.js
@@ -47,36 +47,42 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 
 var controller = (function(budgetCtrl, UICtrl){
-  var DOM = UICtrl.getDOMstrings();
+
+  var setupEventListeners = function(){
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputbtn).addEventListener('click', function(){
+      ctrlAddItem();
+    });
+
+    document.addEventListener('keypress', function(event){
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  
 
   var ctrlAddItem = function(){
       console.log('It works!');
       // get the field input 
       var input = UICtrl.getinput();
-      console.log(input);
+     
       // add the item to the budget controller
 
       // add the item to the UI
 
       // calculate the budget 
 
-      // display the budget on the UI 
-
-     
+      // display the budget on the UI  
   };
 
-  document.querySelector(DOM.inputbtn).addEventListener('click', function(){
-    ctrlAddItem();
-  });
-
-  document.addEventListener('keypress', function(event){
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
-
-
-
-
-
+  return {
+    init: function(){
+      setupEventListeners();
+    },
+  }
 })(budgetController, UIController);
+
+controller.init();
